2d6f8f148811cbf5e238910998748b0eacc0948d
Vanessa Cassandra <vanessa.cassandra@juliusbaer.com>
2021-08-17 09:11:05 +0800

    Add SOLID React demo

--- src/topics/02_SOLID_React/SingleResponsibility/index.jsx
+++ src/topics/02_SOLID_React/SingleResponsibility/index.jsx
@@ -1,14 +1,43 @@
 import React from "react";
-import NavBar from "./NavBar";
-import UserList from "./UserList";
+import styled from "styled-components";
 
+const Header = styled.header`
+  padding: 20px 0;
+  margin-bottom: 10px;
+  border-bottom: solid 1px black;
+  font-weight: bold;
+  text-align: center;
+`;
 
 // Instead of putting all functions in one file, separate them into different components
 const SingleResponsibility = () => {
+  const users = [
+    { id: 1, name: "Leanne", surname: "Graham", age: 20 },
+    { id: 2, name: "Erwin", surname: "Howell", age: 20 },
+  ];
+
   return (
     <>
-      <NavBar />
-      <UserList />
+      <small>src/topics/02_SOLID_React/SingleResponsibility/index.jsx</small>
+      <Header>Header</Header>
+      <table>
+        <thead>
+          <tr>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.surname}</td>
+              <td>{user.age}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
